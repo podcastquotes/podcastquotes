54e7f4675e2922a64768525e495662b2e72d4674
Vincent Schramer <vinciple@gmail.com>
2014-04-09 22:28:03 -0500

    Resolved #2. Configured media/static files for development.

--- podcastquotes/urls.py
+++ podcastquotes/urls.py
@@ -51,5 +51,13 @@ urlpatterns = patterns('',
              ),
         
     url(r'^admin/', include(admin.site.urls)),
-
 )
+
+# Serve static
+from django.conf import settings
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', 
+            {'document_root': settings.MEDIA_ROOT}),
+   )
