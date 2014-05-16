82de8adead9cea3c19a857435aa4ad3e525c0ef2
themitchuation <mitch@everyvote.org>
2014-05-16 02:58:24 -0500

    Enable AJAX voting for mobile xs viewport

--- django_project/core/static/js/ajax-vote.js
+++ django_project/core/static/js/ajax-vote.js
@@ -15,41 +15,51 @@ jQuery(document).ready(function($)
                         {
                         btn.addClass("pq-quote-upvote-active");
                         $('#pq-quote-downvote-' + q_id).removeClass('pq-quote-downvote-active');
+                        $('#pq-quote-xs-downvote-' + q_id).removeClass('pq-quote-downvote-active');
                         var newValue = currentValue + 1;
                         $('#pq-quote-vote-total-' + q_id).text(newValue);
+                        $('#pq-quote-xs-vote-total-' + q_id).text(newValue);
                         }
                     else if (data["newdownvoteobj"])
                         {
                         btn.addClass("pq-quote-downvote-active");
-                        $('#pq-quote-upvote-' + q_id).removeClass('pq-quote-upvote-active')
+                        $('#pq-quote-upvote-' + q_id).removeClass('pq-quote-upvote-active');
+                        $('#pq-quote-xs-upvote-' + q_id).removeClass('pq-quote-upvote-active');
                         var newValue = currentValue - 1;
                         $('#pq-quote-vote-total-' + q_id).text(newValue);
+                        $('#pq-quote-xs-vote-total-' + q_id).text(newValue);
                         }
                     else if (data["upvoteobj"])
                         {
                         btn.addClass("pq-quote-upvote-active");
                         $('#pq-quote-downvote-' + q_id).removeClass('pq-quote-downvote-active');
+                        $('#pq-quote-xs-downvote-' + q_id).removeClass('pq-quote-downvote-active');
                         var newValue = currentValue + 2;
                         $('#pq-quote-vote-total-' + q_id).text(newValue);
+                        $('#pq-quote-xs-vote-total-' + q_id).text(newValue);
                         }
                     else if (data["downvoteobj"])
                         {
                         btn.addClass("pq-quote-downvote-active");
-                        $('#pq-quote-upvote-' + q_id).removeClass('pq-quote-upvote-active')
+                        $('#pq-quote-upvote-' + q_id).removeClass('pq-quote-upvote-active');
+                        $('#pq-quote-xs-upvote-' + q_id).removeClass('pq-quote-upvote-active');
                         var newValue = currentValue - 2;
                         $('#pq-quote-vote-total-' + q_id).text(newValue);
+                        $('#pq-quote-xs-vote-total-' + q_id).text(newValue);
                         }
                     else if (data["un_upvoted"])
                         {
                         btn.removeClass("pq-quote-upvote-active");
                         var newValue = currentValue - 1;
                         $('#pq-quote-vote-total-' + q_id).text(newValue);
+                        $('#pq-quote-xs-vote-total-' + q_id).text(newValue);
                         }
                     else if (data["un_downvoted"])
                         {
                         btn.removeClass("pq-quote-downvote-active");
                         var newValue = currentValue + 1;
                         $('#pq-quote-vote-total-' + q_id).text(newValue);
+                        $('#pq-quote-xs-vote-total-' + q_id).text(newValue);
                         }
                     });
                 btn.attr('disabled', false);
