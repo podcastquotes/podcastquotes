97a86db303a5033a134a181c87f7c5dd280b74c0
Mitch Downey <mitch@everyvote.org>
2014-05-29 02:59:08 -0500

    Fix container padding; delete console.logs

--- django_project/core/static/js/youtube.js
+++ django_project/core/static/js/youtube.js
@@ -3,21 +3,15 @@ for (var i=1; i < 11; i++) {
     videoPlayers.push(document.getElementById("youtube-player-wrapper-" + i));
 }
 
-console.log(videoPlayers);
-
 var startButtons = []
 for (var i=1; i < 11; i++) {
     startButtons.push(document.getElementById("youtube-start-button-wrapper-" + i));
 }
 
-console.log(startButtons);
-
 var xsStartButtons = []
 for (var i=1; i < 11; i++) {
     xsStartButtons.push(document.getElementById("xs-youtube-start-button-wrapper-" + i));
 }
-
-console.log(xsStartButtons);
 
 for (var i=0; i < 10; i++) {
   
@@ -25,23 +19,15 @@ for (var i=0; i < 10; i++) {
   
   if(startButtons[i].childNodes[1] == null) {continue;}
   
-  console.log('it is doing something for ' + forloopCounter);
-  
   var getFullId = startButtons[i].childNodes[1];
-  console.log(getFullId);
   
   var youtubeOnlyId = getFullId.id.substring(0,11);
-  console.log(youtubeOnlyId);
   
   startButtons[i].onclick = function () {
     
     var getStartElement = this.childNodes[1];
-    
-    console.log(getStartElement)
-    
+
     var getPlayerId = getStartElement.childNodes[1].id - 1;
-    
-    console.log(getPlayerId);
 
     // Create an iFrame with autoplay set to true
     var iframe = document.createElement("iframe");
@@ -67,23 +53,15 @@ for (var i=0; i < 10; i++) {
   
   if(xsStartButtons[i].childNodes[1] == null) {continue;}
   
-  console.log('it is doing something for ' + forloopCounter);
-  
   var getFullId = xsStartButtons[i].childNodes[1];
-  console.log(getFullId);
   
   var youtubeOnlyId = getFullId.id.substring(0,11);
-  console.log(youtubeOnlyId);
   
   xsStartButtons[i].onclick = function () {
     
     var getStartElement = this.childNodes[1];
     
-    console.log(getStartElement)
-    
     var getPlayerId = getStartElement.childNodes[1].id - 1;
-    
-    console.log(getPlayerId);
 
     // Create an iFrame with autoplay set to true
     var iframe = document.createElement("iframe");
