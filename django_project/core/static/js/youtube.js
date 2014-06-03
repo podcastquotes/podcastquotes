525152ae90805ac83ff942c177984e93222b2b69
Mitch Downey <mitch@everyvote.org>
2014-06-03 03:26:01 -0500

    UI restyle (in progress)

--- django_project/core/static/js/youtube.js
+++ django_project/core/static/js/youtube.js
@@ -12,13 +12,6 @@ for (var i=1; i < 11; i++) {
 
 console.log(startButtons);
 
-var xsStartButtons = []
-for (var i=1; i < 11; i++) {
-    xsStartButtons.push(document.getElementById("xs-youtube-start-button-wrapper-" + i));
-}
-
-console.log(xsStartButtons);
-
 for (var i=0; i < 10; i++) {
   
   var forloopCounter = i + 1;
@@ -35,40 +28,6 @@ for (var i=0; i < 10; i++) {
     
     var getStartElement = this.childNodes[1];
 
-    var getPlayerId = getStartElement.childNodes[1].id - 1;
-
-    // Create an iFrame with autoplay set to true
-    var iframe = document.createElement("iframe");
-    iframe.setAttribute("src",
-          "https://www.youtube.com/embed/" + getStartElement.id
-        + "autoplay=1&autohide=1&border=0&wmode=opaque&enablejsapi=1");
-    
-    // The height and width of the iFrame should be the same as parent
-    iframe.setAttribute("class", "iframe-size");
-      
-    // Replace the YouTube thumbnail with YouTube HTML5 Player
-    this.style.display = 'none';
-    
-    videoPlayers[getPlayerId].appendChild(iframe);
- 
-  };
-  
-}
-
-for (var i=0; i < 10; i++) {
-  
-  var forloopCounter = i + 1;
-  
-  if(xsStartButtons[i].childNodes[1] == null) {continue;}
-  
-  var getFullId = xsStartButtons[i].childNodes[1];
-  
-  var youtubeOnlyId = getFullId.id.substring(0,11);
-  
-  xsStartButtons[i].onclick = function () {
-    
-    var getStartElement = this.childNodes[1];
-    
     var getPlayerId = getStartElement.childNodes[1].id - 1;
 
     // Create an iFrame with autoplay set to true
