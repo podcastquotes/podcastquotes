1a8fdf0cd3e754e44beb1151de15768082155edb
Mitch Downey <mitch@everyvote.org>
2014-06-14 02:56:43 -0500

    Fix error reporting on login; add superuser_tools permissions

--- django_project/quotes_app/views/superuser_tools.py
+++ django_project/quotes_app/views/superuser_tools.py
@@ -1,3 +1,6 @@
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import user_passes_test
+from django.utils.decorators import method_decorator
 from django.views.generic import DetailView, ListView
 from quotes_app.models import Podcast, Episode
 
@@ -5,6 +8,10 @@ from quotes_app.models import Podcast, Episode
 class NeedYouTubeLinks(ListView):
     model = Episode
     template_name = 'need-youtube-links.html'
+    
+    @method_decorator(staff_member_required)
+    def dispatch(self, *args, **kwargs):
+        return super(NeedYouTubeLinks, self).dispatch(*args, **kwargs)
     
     def get_context_data(self, **kwargs):
         context = super(NeedYouTubeLinks, self).get_context_data(**kwargs)
@@ -15,6 +22,10 @@ class NeedYouTubeLinks(ListView):
 class PodcastEpisodeTitlePrint(ListView):
     model = Podcast
     template_name = 'podcast_episode_title_print.html'
+    
+    @method_decorator(staff_member_required)
+    def dispatch(self, *args, **kwargs):
+        return super(PodcastEpisodeTitlePrint, self).dispatch(*args, **kwargs)
     
     def get_context_data(self, **kwargs):
         context = super(PodcastEpisodeTitlePrint, self).get_context_data(**kwargs)
