16bb6d5cfeb549b39aeef0a9e61552c3fddaad0c
Mitch Downey <mitch@everyvote.org>
2014-07-19 03:52:02 -0500

    Fix update_all_existing_episodes

--- django_project/quotes_app/views/superuser_tools.py
+++ django_project/quotes_app/views/superuser_tools.py
@@ -6,33 +6,12 @@ from django.http import HttpResponseRedirect
 from django.utils.decorators import method_decorator
 from django.views.generic import DetailView, ListView
 from quotes_app.models import Podcast, Episode, Quote, Vote, UserProfile
-    
-# this view checks if every episode with a youtube_url has a full episode clip,
-# if not, it creates one.
-@user_passes_test(lambda u: u.is_staff)
-def create_full_episodes(self):
-    episodes = Episode.objects.all()
-    for episode in episodes:
-        if episode.youtube_url or episode.episode_url:
-            try:
-                full_episode_quote = Quote.objects.get(episode__id=episode.id, is_full_episode=True)
-            except MultipleObjectsReturned:
-                alert('Episode #' + str(episode.id) + 'returns multiple objects!')
-                break
-            except ObjectDoesNotExist:
-                # user with id=1 is the user "podverse" on podverse.tv
-                user = User.objects.get(id=1)
-                full_episode_quote = Quote.create(submitted_by=user,
-                                              rank_score=float(0.0),
-                                              episode=episode,
-                                              summary=episode.title,
-                                              text=episode.description,
-                                              time_quote_begins=int(0),
-                                              is_full_episode=True)
-                full_episode_quote.save()
-                vote = Vote.create(voter=user, quote=full_episode_quote, vote_type=1)
-                vote.save()
-    return HttpResponseRedirect('/')
+
+from django.utils.html import strip_tags
+import feedparser
+import calendar
+from datetime import datetime
+import pytz
 
 # this view checks if every episode with a youtube_url has a full episode clip,
 # if not, it creates one.
@@ -91,6 +70,7 @@ def update_all_existing_episodes(self):
                 pass
             
             episode.save()
+    return HttpResponseRedirect('/')
     
 # this view helps mitch check for episodes which do not have a YouTube link for playing clips
 class NeedYouTubeLinks(ListView):
