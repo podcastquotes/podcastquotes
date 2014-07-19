2200ca5b420b49cd5d9561fd577a93d7c1278f1b
Mitch Downey <mitch@everyvote.org>
2014-07-19 04:02:55 -0500

    Fix update_all_existing_episodes

--- django_project/quotes_app/views/superuser_tools.py
+++ django_project/quotes_app/views/superuser_tools.py
@@ -57,9 +57,6 @@ def update_all_existing_episodes(self):
                 
             episode, created = Episode.objects.get_or_create(podcast_id=podcast_id, guid=e_guid)
             
-            if created == False:
-                continue
-            
             episode.title = e.title
             episode.publication_date = datetime.fromtimestamp(calendar.timegm(e.published_parsed), tz=pytz.utc)
             episode.description = strip_tags(e.description)
