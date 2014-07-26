40acad848cdb905c084467e7690127107281c467
Mitch Downey <mitch@everyvote.org>
2014-07-26 00:50:14 -0500

    Enable YouTube player along with HTML5 audio

--- django_project/core/static/js/players.js
+++ django_project/core/static/js/players.js
@@ -1,8 +1,21 @@
+/* append the YouTube IFRAME API script */
+
+
+var youtube = document.createElement('script');
+youtube.type = "text/javascript";
+youtube.src = "//www.youtube.com/iframe_api";
+
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(youtube, s);
+
 $(window).load(function () {
     var i = 0;
 
     var startButtons = document.getElementsByClassName('pq-slim-quote-picture', i);
+    var mobileStartButtons = document.getElementsByClassName('mobile-start-button', i);
+    var hiddenButton;
     var audioSourceMP3 = [];
+    var YouTubeIDs = [];
     var timeBegins = [];
     var timeEnds = [];
 
@@ -12,7 +25,9 @@ $(window).load(function () {
     
     for (i = 0; i < startButtons.length; ++i) {
         startButtons[i].addEventListener("click", playMedia(i));
+        mobileStartButtons[i].addEventListener("click", playMedia(i));
         audioSourceMP3[i] = startButtons[i].getAttribute('audioSourceMP3');
+        YouTubeIDs[i] = startButtons[i].getAttribute('YouTubeID');
         timeBegins[i] = startButtons[i].getAttribute('timeBegins');
         timeEnds[i] = startButtons[i].getAttribute('timeEnds');
     }
@@ -20,90 +35,96 @@ $(window).load(function () {
     window.playerWrappers = playerWrappers;
     window.openPlayerWrapper = openPlayerWrapper;
     window.startButtons = startButtons;
+    window.mobileStartButtons = mobileStartButtons;
+    window.hiddenButton = hiddenButton;
     window.audioSourceMP3 = audioSourceMP3;
+    window.YouTubeIDs = YouTubeIDs;
     window.timeBegins = timeBegins;
     window.timeEnds = timeEnds;
     
-});
-
-function playMedia(i) {
-    return function () {
-        if (openPlayerWrapper != false) {
-            $('#open-audio').remove();
-        } else {
-            openPlayerWrapper = true;
-        }
-        var audio = document.createElement('audio');
-        audio.setAttribute('controls', '');
-        audio.setAttribute('preload', 'metadata');
-        audio.setAttribute('id', 'open-audio');
-        playerWrappers[i].appendChild(audio);
-
-        var source = document.createElement('source');
-        source.setAttribute('src', audioSourceMP3[i]);
-        audio.appendChild(source);
-        
-        $(playerWrappers[i]).css('margin', '10px 0 5px 0');
-
-        audio.load();
-        audio.onloadedmetadata = function() { 
-            audio.currentTime = timeBegins[i];
-            audio.play();
-        };
-                    
-        audio.addEventListener('timeupdate', function() {
-            if (Math.floor(audio.currentTime) == timeEnds[i]) {
-                audio.pause();
-                audio.currentTime = parseInt(Math.floor(timeEnds[i])) + 1;
+    function playMedia(i) {
+        return function () {
+            if (openPlayerWrapper != false) {
+                $('#open-player').remove();
+                $(hiddenButton).show();
+                hiddenButton = startButtons[i];
+                $(startButtons[i]).hide();
+            } else {
+                openPlayerWrapper = true;
+                hiddenButton = startButtons[i];
+                $(startButtons[i]).hide();
             }
-        },false);
-        
-    }
-}
+            if (YouTubeIDs[i].length == 11) {
+                onYouTubeIframeAPIReady();
+              
+                function onYouTubeIframeAPIReady() {
+                    var YouTubeNode = document.createElement('div');
+                    YouTubeNode.setAttribute('id', 'open-player');
 
-/*
-$(audio).bind('timeupdate', function () {
-    if (audio.currentTime >= timeEnds[i]) { 
-        audio.pause();
+                    playerWrappers[i].appendChild(YouTubeNode);
+                  
+                    playerWrappers[i] = new YT.Player(YouTubeNode, {
+                        playerVars: {'rel': 0, 'showinfo': 0, 'hidecontrols': 1 },
+                        events: {
+                          'onReady': onPlayerReady,
+                          'onStateChange': onPlayerStateChange,
+                        }
+                    });
+                }
+
+                // When the player is ready, we load the video.
+                // loadVideoById starts the video automatically.
+                // cueVideoById does not start the video automatically.
+                  
+                function onPlayerReady(event) {
+                    event.target.loadVideoById({ 
+                        videoId: YouTubeIDs[i],
+                        startSeconds: timeBegins[i],
+                        endSeconds: timeEnds[i]
+                    });
+                  };
+                    
+                  var done = false;
+                  function onPlayerStateChange(event) {
+                      if (event.data == 0) {
+                          done = true;
+                          event.target.destroy();
+                          $(hiddenButton).show();
+                          $('#open-player').remove();
+                      }
+                  };
+                    
+            } else {
+                var audio = document.createElement('audio');
+                audio.setAttribute('controls', '');
+                audio.setAttribute('preload', 'metadata');
+                audio.setAttribute('id', 'open-player');
+                playerWrappers[i].appendChild(audio);
+
+                var source = document.createElement('source');
+                source.setAttribute('src', audioSourceMP3[i]);
+                audio.appendChild(source);
+                
+                $(playerWrappers[i]).css('margin', '10px 0 -5px 0');
+
+                audio.load();
+                audio.onloadedmetadata = function() { 
+                    audio.currentTime = timeBegins[i];
+                    audio.play();
+                };
+                            
+                audio.addEventListener('timeupdate', function() {
+                    if (Math.floor(audio.currentTime) == timeEnds[i]) {
+                        audio.pause();
+                        audio.currentTime = parseInt(Math.floor(timeEnds[i])) + 1;
+                    }
+                },false);
+            }
+        }
     }
+    
 });
-*/
 
-/*
-audio.addEventListener('timeupdate', function() {
-    if (timeEnds[i] && audio.currentTime >= timeEnds[i]) {
-        audio.pause();
-        audio.currentTime = timeBegins[i];
-    }
-},false);
-*/
 
-/*
-var ctx = new webkitAudioContext();
- 
-function loadMusic(url) {
-  var req = new XMLHttpRequest();
-  req.open('GET', 'http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a', true);
-  // req.setRequestHeader('Access-Control-Allow-Origin', '*');
-  req.responseType = 'arraybuffer';
-  // req.setRequestHeader("Range", "bytes=100-200");
-  console.log('here');
-  req.onload = function() {
-    ctx.decodeAudioData(req.response, playSound);
-  };
- console.log('there');
-  req.send();
-}
- 
-function playSound(buffer) {
-  var src = ctx.createBufferSource();
-  src.buffer = buffer;
-  console.log("everywhere");
-  src.connect(ctx.destination);
-  // Play now!
-  src.start(0);
-}
-*/
-
-/* http://traffic.libsyn.com/joeroganexp/p523.mp3 */
-/*http://www.tuxradar.com/files/podcast/tuxradar_s06e02.ogg */
+/* Sample MP3: http://traffic.libsyn.com/joeroganexp/p523.mp3 */
+/* Sample OGG: http://www.tuxradar.com/files/podcast/tuxradar_s06e02.ogg */
